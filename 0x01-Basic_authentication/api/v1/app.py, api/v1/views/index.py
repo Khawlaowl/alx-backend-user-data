97fede652ccc98ddcc6ae3b42a93c62427382b54
Khawlaowl <138932395+Khawlaowl@users.noreply.github.com>
2024-07-10 01:42:12 +0100

    f

--- 0x01-Basic_authentication/api/v1/app.py, api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/app.py, api/v1/views/index.py
@@ -29,8 +29,7 @@ def stats() -> str:
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
-    Return:
-      - raises a 401 error by using abort
+    Return - raises 401 error
     """
     abort(401)
 
@@ -38,7 +37,6 @@ def unauthorized() -> str:
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> str:
     """ GET /api/v1/forbidden
-    Return:
-      - raises a 403 error by using abort
+    Return - raises 403 error
     """
     abort(403)
